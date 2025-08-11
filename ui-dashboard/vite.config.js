4e0368c356a20097d58f0871349db226b9d101e8
Quinten <q.w.j.verkammen2@newcastle.ac.uk>
2025-08-11 10:16:17 +0200

    Change exposed port to match VM public port

--- ui-dashboard/vite.config.js
+++ ui-dashboard/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // allow external connections (needed in Docker)
+    port: 8080       // match your docker-compose port mapping
+  }
 })
