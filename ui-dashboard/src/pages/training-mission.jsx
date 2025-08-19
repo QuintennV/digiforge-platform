651d07278c6ad40446d07a917e6f53a13cf3bcac
AkiraAkiz <natdav201e@gmail.com>
2025-08-19 16:05:50 +0800

    made some changes to training missions

--- ui-dashboard/src/pages/training-mission.jsx
+++ ui-dashboard/src/pages/training-mission.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import '../css/training-mission.css';
 
 function TrainingMissionPage() {
   const location = useLocation();
